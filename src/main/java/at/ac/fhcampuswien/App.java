6abb89a5e28ac553256acceba06c331aee39c204
sabine <sabine.berger@stud.fh-campuswien.ac.at>
2021-12-25 16:15:54 +0100

    Hallo

--- src/main/java/at/ac/fhcampuswien/App.java
+++ src/main/java/at/ac/fhcampuswien/App.java
@@ -7,7 +7,7 @@ import javafx.scene.layout.*;
 import javafx.stage.*;
 
 public class App extends Application {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         launch(args);
     }
 
@@ -16,13 +16,13 @@ public class App extends Application {
         primaryStage.setTitle("Hello World!");
         Button btn = new Button();
         btn.setText("Hello JavaFX!");
-        btn.setOnAction( (event) -> Platform.exit() );
+        btn.setOnAction((event) -> Platform.exit());
         Pane root = new StackPane();
         root.getChildren().add(btn);
         primaryStage.setScene(new Scene(root, 300, 150));
         primaryStage.show();
 
         System.out.print("Hallo Adam!");
-        System.out.print("Es klappt")
+        System.out.print("Es klappt");
     }
 }
