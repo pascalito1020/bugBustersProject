513cf3b9b8c30fce495b3bedc8a8bd75f3d516b0
Christian17f <christian.fichtenbauer@stud.fh-campuswien.ac.at>
2022-01-06 10:58:40 +0100

    classes defined

---
+++ src/main/java/at/ac/fhcampuswien/Sound.java
@@ -0,0 +1,16 @@
+package at.ac.fhcampuswien;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class Sound extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+
+    }
+}
